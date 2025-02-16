7d10a837bb737bca1d9cd6090a31ef3b1dd13012
huyennt <huyennt101@ghtk.co>
2025-02-16 11:10:32 +0700

    clean code

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"go-rest-api/config"
 	"go-rest-api/controllers"
-	"go-rest-api/repo"
 	"go-rest-api/routes"
 	"go-rest-api/service"
 	"log"
@@ -17,16 +16,12 @@ func main() {
 		}
 	}()
 	config.LoadConfig("")
-	config.ConnectDatabase()
 	// Initialize repository, service, and controller
-	productRepo := repo.NewProductRepository(config.DB)
-	productService := service.NewProductService(productRepo)
-	productController := controllers.NewProductController(productService)
 
 	videoCallService := service.NewVideoCallService()
 	videoController := controllers.NewWebRtcController(videoCallService)
 
-	r := routes.NewRoute(productController, videoController)
+	r := routes.NewRoute(videoController)
 	port := config.AppConfig.App.Port
 	log.Println("server run in: " + port)
 	err := r.Run(":" + port)
--- routes/routes.go
+++ routes/routes.go
@@ -7,7 +7,7 @@ import (
 	"go-rest-api/middlewares"
 )
 
-func NewRoute(productApi *api.ProductController, rtcApi *api.WebRtcController) *gin.Engine {
+func NewRoute(rtcApi *api.WebRtcController) *gin.Engine {
 	r := gin.Default()
 
 	// Register the IPLogger middleware
@@ -21,12 +21,7 @@ func NewRoute(productApi *api.ProductController, rtcApi *api.WebRtcController) *
 	// Increase the maximum request body size to 10 MB
 	r.MaxMultipartMemory = 10 << 20 // 10 MB (dịch bit)
 
-	r.GET("/products", productApi.FindProductsHandler)
-	r.POST("/products", api.CreateProduct)
-	r.GET("/products/:id", api.FindProduct)
-	r.PUT("/products/:id", api.UpdateProduct)
-	r.DELETE("/products/:id", api.DeleteProduct)
-	// webrtc
-	r.POST("/webrtc/sdp/m/:meetingId/c/:userID/p/:peerID/s/:isSender", rtcApi.MakeVideoCallHandler)
+	// webrtc client control
+	r.POST("/remote/:roomId", rtcApi.MakeVideoCallHandler)
 	return r
 }
