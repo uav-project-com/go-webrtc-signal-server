a38d37fa18eb6edf26d707fdfec2e8acae4deb58
einsteinarbert <hieu19926@gmail.com>
2025-02-18 01:21:14 +0700

    draff-data-channel

--- ui/src/app/call/call.component.ts
+++ ui/src/app/call/call.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Sdp} from './Sdp';
-import {ActivatedRoute} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Sdp } from './Sdp';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
@@ -15,7 +15,9 @@ export class CallComponent implements OnInit {
 
   pcSender: any
   pcReceiver: any
-  private dataChannel: RTCDataChannel | null = null;
+  data2WaySender: any
+  dataChannel: RTCDataChannel | null = null;
+
   meetingId: string
   peerID: string
   userId: string
@@ -38,8 +40,11 @@ export class CallComponent implements OnInit {
       iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
     };
 
+    // video is one-way, each connection is independent and works separately: { direction: 'recvonly' }
     this.pcSender = new RTCPeerConnection(config)
     this.pcReceiver = new RTCPeerConnection(config)
+    // Unlike video, DataChannel requires a bidirectional connection:
+    this.data2WaySender = new RTCPeerConnection(config)
 
     // create data channel
     this.setupDataConnections();
@@ -48,15 +53,27 @@ export class CallComponent implements OnInit {
   }
 
   startCall() {
+    // creating webrtc datachannel connection
+    this.data2WaySender.createOffer().then((d: any) => {
+      this.data2WaySender.setLocalDescription(d)
+      console.log(new Date() + ' data2WaySender.createOffer')
+    })
+    this.data2WaySender.addEventListener('connectionstatechange', _event => {
+      console.log('connectionstatechange-state:' + this.data2WaySender.connectionState)
+      if (this.data2WaySender.connectionState === 'connected') {
+        console.log('datachannel connected!')
+      }
+    });
+
     // sender part of the call
-    navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((stream) => {
       const senderVideo: any = document.getElementById('senderVideo');
       senderVideo.srcObject = stream;
       const tracks = stream.getTracks();
       for (let i = 0; i < tracks.length; i++) {
         this.pcSender.addTrack(stream.getTracks()[i]);
       }
-      this.pcSender.createOffer().then(d => {
+      this.pcSender.createOffer().then((d: any) => {
         this.pcSender.setLocalDescription(d)
         console.log(new Date() + ' pcSender.createOffer')
       })
@@ -70,7 +87,7 @@ export class CallComponent implements OnInit {
     });
 
     // receiver part of the call
-    this.pcReceiver.addTransceiver('video', {direction: 'recvonly'})
+    this.pcReceiver.addTransceiver('video', { direction: 'recvonly' })
 
     this.pcReceiver.createOffer()
       .then((d: any) => {
@@ -99,14 +116,37 @@ export class CallComponent implements OnInit {
   }
 
   private setupVideoConnections() {
+    this.data2WaySender.onicecandidate = (event) => {
+      if (event.candidate) {
+        // Send ICE candidate to remote peer via HTTP signaling
+        this.http.post('http://127.0.0.1:8080/webrtc/candidate/m/' 
+          + this.meetingId + '/c/' + this.userId + '/p/' + this.peerID,
+          { candidate: btoa(JSON.stringify(event.candidate)) }).subscribe();
+      }
+    };
+    
+    // Once ICE gathering is complete, send SDP offer
+    this.data2WaySender.onicegatheringstatechange = () => {
+      if (this.data2WaySender.iceGatheringState === "complete") {
+        console.log(new Date() + " data2WaySender HTTP POST SDP");
+        this.http.post<Sdp>('http://127.0.0.1:8080/webrtc/sdp/m/'
+          + this.meetingId + '/c/' + this.userId + '/p/' + this.peerID,
+          { sdp: btoa(JSON.stringify(this.data2WaySender.localDescription)) })
+          .subscribe(response => {
+            this.data2WaySender.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(response.Sdp))));
+          });
+      }
+    };
+    
+
     this.pcSender.onicecandidate = (event: { candidate: null; }) => {
       if (event.candidate === null) {
         console.log(new Date() + ' sender http.post')
         this.http.post<Sdp>('http://127.0.0.1:8080/webrtc/sdp/m/'
           + this.meetingId + '/c/' + this.userId + '/p/' + this.peerID + '/s/' + true,
-          {sdp: btoa(JSON.stringify(this.pcSender.localDescription))}).subscribe(response => {
-          this.pcSender.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(response.Sdp))))
-        });
+          { sdp: btoa(JSON.stringify(this.pcSender.localDescription)) }).subscribe(response => {
+            this.pcSender.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(response.Sdp))))
+          });
       }
     }
     this.pcReceiver.onicecandidate = (event: { candidate: null; }) => {
@@ -114,29 +154,37 @@ export class CallComponent implements OnInit {
         console.log(new Date() + ' receiver http.post')
         this.http.post<Sdp>('http://127.0.0.1:8080/webrtc/sdp/m/'
           + this.meetingId + '/c/' + this.userId + '/p/' + this.peerID + '/s/' + false,
-          {sdp: btoa(JSON.stringify(this.pcReceiver.localDescription))}).subscribe(response => {
-          this.pcReceiver.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(response.Sdp))))
-        })
+          { sdp: btoa(JSON.stringify(this.pcReceiver.localDescription)) }).subscribe(response => {
+            this.pcReceiver.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(response.Sdp))))
+          })
       }
     }
   }
 
   private setupDataConnections() {
-    this.dataChannel = this.pcSender.createDataChannel('chat');
+    // must init before create channel for listen event of create channel `chat`
+    this.data2WaySender.ondatachannel = (event) => {
+      console.log("Data channel received on receiver");
+      this.dataChannel = event.channel; // Store the data channel reference
 
-    this.dataChannel.onopen = () => console.log('Data channel opened!');
+      this.dataChannel.onmessage = (messageEvent) => {
+        console.log("Received message from sender:", messageEvent.data);
+        this.receivedMessages.push("Peer: " + messageEvent.data);
+        this.cdr.detectChanges();
+      };
+
+      this.dataChannel.onopen = () => console.log("Receiver data channel is open");
+    };
+
+    // Create sender's data channel
+    this.dataChannel = this.data2WaySender.createDataChannel("chat");
+
+    this.dataChannel.onopen = () => console.log("Data channel opened!");
     this.dataChannel.onmessage = (event) => {
-      console.log('Received message1:', event.data)
-      this.receivedMessages.push('Peer1: ' + event.data)
-      this.cdr.detectChanges()
-    }
-
-    this.pcReceiver.ondatachannel = (event) => {
-      event.channel.onmessage = (messageEvent) => {
-        console.log('Received message2:', messageEvent.data)
-        this.receivedMessages.push('Peer2: ' + messageEvent.data)
-        this.cdr.detectChanges()
-      }
-    }
+      console.log("Received message:", event.data);
+      this.receivedMessages.push("You: " + event.data);
+      this.cdr.detectChanges();
+    };
   }
+
 }
