bef326778aa517552f815837880a92aa046544a1
huyennt <huyennt101@ghtk.co>
2025-12-11 01:10:22 +0700

    feat(draft): init golang client UAV project - user-service OK la

--- ui/go-client/main.go
+++ ui/go-client/main.go
@@ -1,83 +1,92 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	jwt "github.com/appleboy/gin-jwt/v2"
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/uav-project-com/go-webrtc-signal-server/go-rtc-client/service"
-	"time"
+  "crypto/sha256"
+  "encoding/hex"
+  "log"
+  "time"
+
+  jwt "github.com/appleboy/gin-jwt/v2"
+  "github.com/gin-gonic/gin"
+  "github.com/google/uuid"
+  "github.com/uav-project-com/go-webrtc-signal-server/go-rtc-client/service"
 )
 
 type Login struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
+  Username string `form:"username" json:"username" binding:"required"`
+  Password string `form:"password" json:"password" binding:"required"`
 }
 
 func hashSHA256(password string) string {
-	// Chuyển chuỗi mật khẩu thành slice byte
-	hash := sha256.New()
-	hash.Write([]byte(password))
+  // Chuyển chuỗi mật khẩu thành slice byte
+  hash := sha256.New()
+  hash.Write([]byte(password))
 
-	// Tính toán giá trị hash và trả về kết quả dưới dạng chuỗi hex
-	return hex.EncodeToString(hash.Sum(nil))
+  // Tính toán giá trị hash và trả về kết quả dưới dạng chuỗi hex
+  return hex.EncodeToString(hash.Sum(nil))
 }
 
 func configRoutes() *gin.Engine {
-	r := gin.Default()
-	uuidString := uuid.New().String()
-	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-		Realm:      "example zone",
-		Key:        []byte(uuidString + "eb43b856"),
-		Timeout:    time.Hour,
-		MaxRefresh: time.Hour,
-		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var loginVals Login
-			if err := c.ShouldBind(&loginVals); err != nil {
-				return "", jwt.ErrMissingLoginValues
-			}
+  r := gin.Default()
+  uuidString := uuid.New().String()
+  authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+    Realm:      "example zone",
+    Key:        []byte(uuidString + "eb43b856"),
+    Timeout:    time.Hour,
+    MaxRefresh: time.Hour,
+    Authenticator: func(c *gin.Context) (interface{}, error) {
+      var loginVals Login
+      if err := c.ShouldBind(&loginVals); err != nil {
+        return "", jwt.ErrMissingLoginValues
+      }
 
-			userID := loginVals.Username
-			password := loginVals.Password
-			hashPassword := hashSHA256(password)
+      userID := loginVals.Username
+      password := loginVals.Password
+      hashPassword := hashSHA256(password)
 
-			// Validate username/password
-			user, err := service.FindByUsername(userID)
-			if err != nil {
-				return nil, jwt.ErrFailedAuthentication
-			}
-			if user.Username == "admin" && hashPassword == user.Password {
-				return &Login{Username: userID}, nil
-			}
-			return nil, jwt.ErrFailedAuthentication
-		},
-		IdentityHandler: func(c *gin.Context) interface{} {
-			claims := jwt.ExtractClaims(c)
-			return claims["id"]
-		},
-	})
+      // Validate username/password
+      user, err := service.FindByUsername(userID)
+      if err != nil {
+        log.Printf("FindByUsername error: %v\n", err)
+        return nil, jwt.ErrFailedAuthentication
+      }
+      if user.Username == "admin" && hashPassword == user.Password {
+        return &Login{Username: userID}, nil
+      }
+      return nil, jwt.ErrFailedAuthentication
+    },
+    IdentityHandler: func(c *gin.Context) interface{} {
+      claims := jwt.ExtractClaims(c)
+      return claims["id"]
+    },
+  })
 
-	if err != nil {
-		panic(err)
-	}
+  if err != nil {
+    panic(err)
+  }
 
-	r.POST("/login", authMiddleware.LoginHandler)
-	r.GET("/refresh", authMiddleware.RefreshHandler)
+  r.POST("/login", authMiddleware.LoginHandler)
+  r.GET("/refresh", authMiddleware.RefreshHandler)
 
-	auth := r.Group("/auth")
-	auth.Use(authMiddleware.MiddlewareFunc())
-	auth.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello from protected route"})
-	})
-	return r
+  auth := r.Group("/auth")
+  auth.Use(authMiddleware.MiddlewareFunc())
+  auth.GET("/hello", func(c *gin.Context) {
+    c.JSON(200, gin.H{"message": "Hello from protected route"})
+  })
+  return r
 }
 
 func main() {
-	router := configRoutes()
+  log.SetFlags(log.LstdFlags | log.Lshortfile) // Enable file:line logging
+  defer func() { // keep server live when panic error occurred
+    if err := recover(); err != nil {
+      log.Printf("Recovered from panic: %v", err)
+    }
+  }()
+  router := configRoutes()
 
-	err := router.Run(":8080")
-	if err != nil {
-		return
-	}
+  err := router.Run(":8080")
+  if err != nil {
+    return
+  }
 }
--- ui/go-client/service/userservice.go
+++ ui/go-client/service/userservice.go
@@ -1,43 +1,39 @@
 package service
 
 import (
-	"database/sql"
-	"fmt"
+  "database/sql"
+  "fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+  _ "github.com/mattn/go-sqlite3"
 )
 
 // User represents a record in the `user` table from the SQLite database.
 type User struct {
-	ID       int64  `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+  ID       int64  `json:"id"`
+  Username string `json:"username"`
+  Password string `json:"password"`
 }
 
 // FindByUsername opens the sqlite database file at dbPath (e.g. `/tmp/db.data`) and
 // returns row from the `user` table selecting columns `ID, username, password`.
 func FindByUsername(username string) (*User, error) {
-	db, err := sql.Open("sqlite3", "X:\\workspace\\0.FPV\\go-webrtc-signal-server\\ui\\go-client\\SQLite.db")
-	if err != nil {
-		return nil, fmt.Errorf("open sqlite db: %w", err)
-	}
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			fmt.Printf("close sqlite db: %v", err)
-		}
-	}(db)
+  db, err := sql.Open("sqlite3", "/mnt/x/workspace/0.FPV/go-webrtc-signal-server/ui/go-client/SQLite.db")
+  if err != nil {
+    return nil, fmt.Errorf("open sqlite db: %w", err)
+  }
+  defer func(db *sql.DB) {
+    err := db.Close()
+    if err != nil {
+      fmt.Printf("close sqlite db: %v", err)
+    }
+  }(db)
 
-	row, err := db.Query("SELECT ID, username, password FROM user where username = ?", username)
-	if err != nil {
-		return nil, fmt.Errorf("query user table: %w", err)
-	}
-
-	var user User
-	// Scan vào các biến tương ứng
-	err = row.Scan(&user)
-	if err != nil {
-		return nil, fmt.Errorf("row error: %w", err)
-	}
-	return &user, nil
+  row := db.QueryRow("SELECT ID, username, password FROM user where username = ?", username)
+  var user User
+  // Scan vào các biến tương ứng
+  err = row.Scan(&user.ID, &user.Username, &user.Password)
+  if err != nil {
+    return nil, fmt.Errorf("row error: %w", err)
+  }
+  return &user, nil
 }
